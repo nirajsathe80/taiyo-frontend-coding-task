fc5f1f3f346016ff7dd7c084a44afd1b576d4b52
nirajsathe80 <nirajsathe80@gmail.com>
2024-08-18 16:39:57 +0530

    types file add

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+export interface ContatcInitialState {
+  contactList: IContact[];
+}
+
+export interface FormValue {
+  firstName: string;
+  lastName: string;
+  isActive: string;
+}
+
+export interface IContact extends FormValue {
+  id: string;
+}
+
+export interface IAllCovidCases {
+  updated: number;
+  cases: number;
+  todayCases: number;
+  deaths: number;
+  todayDeaths: number;
+  recovered: number;
+  todayRecovered: number;
+  active: number;
+  critical: number;
+  casesPerOneMillion: number;
+  deathsPerOneMillion: number;
+  tests: number;
+  testsPerOneMillion: number;
+  population: number;
+  oneCasePerPeople: number;
+  oneDeathPerPeople: number;
+  oneTestPerPeople: number;
+  activePerOneMillion: number;
+  recoveredPerOneMillion: number;
+  criticalPerOneMillion: number;
+  affectedCountries: number;
+}
