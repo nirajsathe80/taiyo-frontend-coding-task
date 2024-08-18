5bd84f7abf0a3d7792412daa7f4e7f53f5e038d3
nirajsathe80 <nirajsathe80@gmail.com>
2024-08-18 16:44:58 +0530

    Homepage create

---
+++ src/pages/homepage/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "../../layout";
+
+function HomePage() {
+  return (
+    <Layout>
+      <div className="font-bold text-4xl flex justify-center items-center h-screen">
+        Taiyo Homepage
+      </div>
+    </Layout>
+  );
+}
+
+export default HomePage;
