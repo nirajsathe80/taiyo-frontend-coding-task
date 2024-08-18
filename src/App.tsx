1daf285dd6d0dfaa657f922e81652b20e50fa6e5
nirajsathe80 <nirajsathe80@gmail.com>
2024-08-18 16:41:09 +0530

    Required path add

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Loader from "./components/loader";
 
-function App() {
-  return <div>Hello</div>;
-}
+const HomePage = lazy(() => import("../src/pages/homepage"));
+const Contact = lazy(() => import("../src/pages/contact"));
+const NewContact = lazy(() => import("./pages/contact/new-contact"));
+const EditContact = lazy(() => import("./pages/contact/edit-contact"));
+const ViewContact = lazy(() => import("./pages/contact/view-contact"));
+const ChartAndMaps = lazy(() => import("./pages/charAndMaps"));
+
+const App = function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact/new" element={<NewContact />} />
+          <Route path="/contact/:id/edit" element={<EditContact />} />
+          <Route path="/contact/:id" element={<ViewContact />} />
+          <Route path="/charts-and-maps" element={<ChartAndMaps />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
 
 export default App;
