41a780133db0fab4e252521a2f8cb356626091bf
nirajsathe80 <nirajsathe80@gmail.com>
2024-08-18 16:42:59 +0530

    Common Layout folder add

---
+++ src/layout/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "../components/sidebar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex md:flex-row flex-col">
+      <div className="md:w-[25%] w-[100vw]">
+        <Sidebar />
+      </div>
+      <div className="bg-[#efece3] min-h-[100vh]  md:w-[75%] w-full">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
